21df4131ca76bbc0be4cbbfd3f080299e000e5f3
Karen Rodriguez Macias <KarenRoMa@UNAM-MOBILE.local>
2015-09-19 10:33:21 -0500

    FirstCommit

---
+++ Lotería/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lotería
+//
+//  Created by Karen Rodriguez Macias on 19/09/15.
+//  Copyright © 2015 Karen Rodriguez Macias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ LoteríaTests/Loteri_aTests.swift
@@ -0,0 +1,36 @@
+//
+//  Loteri_aTests.swift
+//  LoteríaTests
+//
+//  Created by Karen Rodriguez Macias on 19/09/15.
+//  Copyright © 2015 Karen Rodriguez Macias. All rights reserved.
+//
+
+import XCTest
+@testable import Loteri_a
+
+class Loteri_aTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
