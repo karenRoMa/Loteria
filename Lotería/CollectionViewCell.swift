4b442de8b9b04fb33e392de5d198ae495de3babc
vicenteguerra <vicenteguerrahdez@gmail.com>
2015-09-20 23:18:29 -0500

    Collection View with Images (Player View)

---
+++ Lotería/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  Lotería
+//
+//  Created by Vicente Guerra Hernández on 9/20/15.
+//  Copyright © 2015 Karen Rodriguez Macias. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
